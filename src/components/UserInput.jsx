bf632db723ae917f9dec70d9c2be2a5dd1210941
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-12-03 10:46:26 -0800

    updated results component; lifted state

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,21 +1,7 @@
-import { useState } from 'react';
+export default function UserInput({onChange, userInput}) {
 
-export default function UserInput() {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10
-  });
 
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput((prevUserInput) => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: newValue,
-      };
-    });
-  }
+ 
 
   return (
     <section id="user-input">
@@ -27,7 +13,7 @@ export default function UserInput() {
             required
             value={userInput.initialInvestment}
             onChange={(event) =>
-              handleChange('initialInvestment', event.target.value)
+              onChange('initialInvestment', event.target.value)
             }
           />
         </p>
@@ -38,7 +24,7 @@ export default function UserInput() {
             required
             value={userInput.annualInvestment}
             onChange={(event) =>
-              handleChange('annualInvestment', event.target.value)
+              onChange('annualInvestment', event.target.value)
             }
           />
         </p>
@@ -51,7 +37,7 @@ export default function UserInput() {
             required
             value={userInput.expectedReturn}
             onChange={(event) =>
-              handleChange('expectedReturn', event.target.value)
+              onChange('expectedReturn', event.target.value)
             }
           />
         </p>
@@ -62,7 +48,7 @@ export default function UserInput() {
             required
             value={userInput.duration}
             onChange={(event) =>
-              handleChange('duration', event.target.value)
+              onChange('duration', event.target.value)
             }
           />
         </p>
