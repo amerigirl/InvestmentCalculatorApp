8cca6b873c8bc296a30e303eb981a98ce8a46465
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-01-03 17:08:58 -0800

    centering and adding helpfulhints image

--- src/components/HelpfulHints/HelpfulHints.jsx
+++ src/components/HelpfulHints/HelpfulHints.jsx
@@ -1,9 +1,10 @@
 import './HelpfulHints.styles.css';
+import dreamVacay from '../../assets/dreamVacay.jpeg';
 
 export default function HelpfulHints(){
     return (
-        <div className='helpful-hints'>
-  Placeholder text
+        <div id='helpful-hints'>
+            <img id="pic" src={dreamVacay} alt='Dream vacation' />
         </div>
     );
 }
