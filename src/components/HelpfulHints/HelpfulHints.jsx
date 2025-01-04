7116ae44ba9b5719ca8277aa30caa6bd52e260d4
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-01-03 17:38:38 -0800

    adding text

--- src/components/HelpfulHints/HelpfulHints.jsx
+++ src/components/HelpfulHints/HelpfulHints.jsx
@@ -1,10 +1,17 @@
-import './HelpfulHints.styles.css';
-import dreamVacay from '../../assets/dreamVacay.jpeg';
+import "./HelpfulHints.styles.css";
+import dreamVacay from "../../assets/dreamVacay.jpeg";
 
-export default function HelpfulHints(){
-    return (
-        <div id='helpful-hints'>
-            <img id="pic" src={dreamVacay} alt='Dream vacation' />
-        </div>
-    );
+export default function HelpfulHints() {
+  return (
+    <>
+      <p id="dollar">See what a difference a dollar can make!</p>
+      <p id="vacay">
+        Your next vacation is only<br></br> a few $dollars$ away!
+      </p>
+
+      <div id="helpful-hints">
+        <img id="pic" src={dreamVacay} alt="Dream vacation" />
+      </div>
+    </>
+  );
 }
